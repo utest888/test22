910028edebc0be4bcfcce0e5c49c2036c66103ba
utest888 <utest888@qq.com>
2020-11-21 00:37:25 +0000

    用户注册界面

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,12 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title','Weibo') - 第3次完成</title>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 
 <body>
 
-    @yield('content')
 
+    @include('layouts._header')
+    <div class="container">
+        <div class="offset-md-1 col-md-10">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </div>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 
 </html>
